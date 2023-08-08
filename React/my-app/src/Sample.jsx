8cc5ada70484c0844ff5a8106ce71990662285ac
AMZ <80452444+amegh-amz@users.noreply.github.com>
2023-08-08 12:50:26 +0530

    08-08-2023

---
+++ React/my-app/src/Sample.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sample2 from './Sample2'
+
+function Sample() {
+  return (
+    <div>
+        <h1>Lorem ipsum dolor sit.</h1>
+        <Sample2/>
+    </div>
+  )
+}
+
+export default Sample
