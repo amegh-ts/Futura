70e773166d8e9b7d5805486c4641bd6e51182cfc
AMZ <80452444+amegh-amz@users.noreply.github.com>
2023-08-13 17:05:19 +0530

    13-08-2023

---
+++ React/Works/login-register/src/App.js
@@ -0,0 +1,15 @@
+
+import Login from './Login';
+import SignUp from './SignUp';
+
+function App() {
+  return (
+    <div className="App">
+      <Login/>
+      <br></br>
+      <SignUp/>
+    </div>
+  );
+}
+
+export default App;
