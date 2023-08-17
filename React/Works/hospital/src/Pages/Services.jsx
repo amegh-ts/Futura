2983901a5df606875b9ab6fe3fefc35e4469e3ec
AMZ <80452444+amegh-amz@users.noreply.github.com>
2023-08-17 11:28:12 +0530

    17-08-2023

---
+++ React/Works/hospital/src/Pages/Services.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Services() {
+  return (
+    <div >
+      <h1>Our Services</h1>
+      <ul>
+        <li>Emergency Care</li>
+        <li>Surgery</li>
+        <li>Maternity</li>
+        <li>Diagnostic Imaging</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Services;
