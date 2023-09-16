4624b97a62977d64df68ceaf0b6bbe67019e75e9
AMZ <80452444+amegh-amz@users.noreply.github.com>
2023-09-16 11:36:01 +0530

    16-09-2023

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,48 +1,85 @@
-// import HookUseState from "./Pages/HookUseState";
-// import InternalStyling from "./Pages/InternalStyling";
-// import Sample from "./Pages/Sample";
-// import StyledComponent from "./Pages/StyledComponent";
-// import Api from "./Pages/Api";
-// import ColourButton2 from "./Pages/Button/ColourButton2";
-// import ColourButton3 from "./Pages/Button/ColourButton3";
-// import ColourButton4 from "./Pages/Button/ColourButton4";
-// import ColourButtons from "./Pages/Button/ColourButtons";
-// import { HookUseContext } from "./Pages/HookUseContext";
-// import HookUseContextDisplay from "./Pages/HookUseContextDisplay";
-// import UseRef from "./Pages/UseRef";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import HookUseState from "./Pages/HookUseState";
+import InternalStyling from "./Pages/InternalStyling";
+import Sample from "./Pages/Sample";
+import StyledComponent from "./Pages/StyledComponent";
+import Api from "./Pages/Api";
+import ColourButton2 from "./Pages/Button/ColourButton2";
+import ColourButton3 from "./Pages/Button/ColourButton3";
+import ColourButton4 from "./Pages/Button/ColourButton4";
+import ColourButtons from "./Pages/Button/ColourButtons";
+import { HookUseContext } from "./Pages/HookUseContext";
+import HookUseContextDisplay from "./Pages/HookUseContextDisplay";
+import UseRef from "./Pages/UseRef";
 
-import TryCatch from "./Pages/TryCatch";
+// import TryCatch from "./Pages/TryCatch";
+import UseReducer from "./Pages/UseReducer";
 
 // import UseEffect from "./Pages/UseEffect";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: 'UseReducer',
+      element: <UseReducer />
+    },
+    {
+      path: 'Sample',
+      element: <Sample />
+    },
+    {
+      path: 'InternalStyling',
+      element: <InternalStyling />
+    }, 
+    {
+      path: 'StyledComponent',
+      element: <StyledComponent />
+    }, 
+    {
+      path: 'HookUseState',
+      element: <HookUseState />
+    }, 
+    {
+      path: 'UseRef',
+      element: <UseRef />
+    }, 
+    // {
+    //   path: 'UseEffect',
+    //   element: <UseEffect />
+    // }, 
+    {
+      path: 'ColourButtons',
+      element: <ColourButtons />
+    }, 
+    {
+      path: 'Api',
+      element: <Api />
+    },
+  ])
   return (
-    <div className="App">
+   
 
-      {/* <Sample/>
-      <br/>
-      <InternalStyling/>
-      <br/> 
-      <StyledComponent/>
-      <HookUseState/> */}
+      
 
-      {/* <HookUseContext.Provider value={'kratos'}>
+
+    //   {/* <ColourButtons/><br/><br/><br/><br/>
+    //   <ColourButton2/><br/><br/><br/><br/>
+    //   <ColourButton3/><br/><br/><br/><br/>
+    //   <ColourButton4/> */}
+
+    //   {/* <Api/> */}
+
+    //   {/* <TryCatch/> */}
+
+    //   // <UseReducer/>
+    // </div>
+
+    <HookUseContext.Provider value={'kratos'}>
       <HookUseContextDisplay/>
-      </HookUseContext.Provider> */}
+    <RouterProvider router={router}></RouterProvider>
 
-      {/* <UseRef/> */}
-      {/* <UseEffect/> */}
-    
+      </HookUseContext.Provider>
 
-      {/* <ColourButtons/><br/><br/><br/><br/>
-      <ColourButton2/><br/><br/><br/><br/>
-      <ColourButton3/><br/><br/><br/><br/>
-      <ColourButton4/> */}
-
-      {/* <Api/> */}
-
-      <TryCatch/>
-    </div>
   );
 }
 
---
+++ React/my-app/src/Pages/UseReducer.jsx
@@ -0,0 +1,56 @@
+import React, { useReducer } from 'react'
+import { Link } from 'react-router-dom';
+
+function UseReducer() {
+
+    const InitialState = 0
+    const reducer = (state, action) => {
+        console.log('action', action, 'state', state);
+        console.log(action);
+        // switch (action.type) {
+        //     case 'increase':
+        //         return state = state + 1
+        //     case 'decrease':
+        //         return state = state > 0 ? state - 1 : 0
+        //     case 'increment2':
+        //         return state = state + 2
+        //     case 'decrement2':
+        //         return state = state >= 2 ? state - 2 : 0
+        //     case 'reset':
+        //         return InitialState;
+        //     default:
+        //         return state
+        // }
+
+        // using if else
+        if (action.type === 'increase') {
+            return state + 1;
+        } else if (action.type === 'decrease') {
+            return state > 0 ? state - 1 : 0;
+        } else if (action.type === 'increment2') {
+            return state + 2;
+        } else if (action.type === 'decrement2') {
+            return state >= 2 ? state - 2 : 0;
+        } else if (action.type === 'reset') {
+            return InitialState;
+        } else {
+            return state;
+        }
+    }
+
+    const [state, dispatch] = useReducer(reducer, InitialState)
+
+    return (
+        <div>
+            <h1>{state}</h1>
+            <button onClick={() => dispatch({ type: 'increase' })}>Increment</button>
+            <button onClick={() => dispatch({ type: 'decrease' })}>decrease</button>
+            <button onClick={() => dispatch({ type: 'increment2' })}>increment2</button>
+            <button onClick={() => dispatch({ type: 'decrement2' })}>decrement2</button>
+            <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
+            <Link to={'Sample'}><h1>heeeeee</h1></Link>
+        </div>
+    )
+}
+
+export default UseReducer
