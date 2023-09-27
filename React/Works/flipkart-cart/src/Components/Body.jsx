4cb6ab326377a6fb34e124ce8599af51848fced5
AMZ <80452444+amegh-amz@users.noreply.github.com>
2023-09-27 22:08:26 +0530

    27-09-2023

--- React/Works/flipkart-cart/src/Components/Body.jsx
+++ React/Works/flipkart-cart/src/Components/Body.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import cardimg1 from './Assets/card-img-1.png'
 import cardimg2 from './Assets/card-img-2.png'
 
 const Body = () => {
+    const[state, setState]=useState([]);
+    useEffect(()=>{
+        async function name(params) {
+            
+        }
+    })
   return (
     <div>
         <div className="card-container">
@@ -21,61 +27,7 @@ const Body = () => {
 
                     </div>
 
-                    <div className="card-card">
-                        <span className="card-text">2</span>
-                        <img
-                            src={cardimg1}
-                            alt="grocerygettyf1122"
-                            className="card-grocerygettyf1"
-                        />
-                        <span className="card-text1"><span>vegies</span></span>
-                        <span className="card-text3"><span>Price : $20</span></span>
-                        <button className='card-add-to-cart-button'>
-                            <span className="card-text5">Add To Cart</span>
-                        </button>
-                    </div>
-
-                    <div className="card-card">
-                        <span className="card-text">3</span>
-                        <img
-                            src={cardimg1}
-                            alt="grocerygettyf1122"
-                            className="card-grocerygettyf1"
-                        />
-                        <span className="card-text1"><span>vegies</span></span>
-                        <span className="card-text3"><span>Price : $20</span></span>
-                        <button className='card-add-to-cart-button'>
-                            <span className="card-text5">Add To Cart</span>
-                        </button>
-                    </div>
-
-                    <div className="card-card">
-                        <span className="card-text">4</span>
-                        <img
-                            src={cardimg1}
-                            alt="grocerygettyf1122"
-                            className="card-grocerygettyf1"
-                        />
-                        <span className="card-text1"><span>vegies</span></span>
-                        <span className="card-text3"><span>Price : $20</span></span>
-                        <button className='card-add-to-cart-button'>
-                            <span className="card-text5">Add To Cart</span>
-                        </button>
-                    </div>
-
-                    <div className="card-card">
-                        <span className="card-text">5</span>
-                        <img
-                            src={cardimg1}
-                            alt="grocerygettyf1122"
-                            className="card-grocerygettyf1"
-                        />
-                        <span className="card-text1"><span>vegies</span></span>
-                        <span className="card-text3"><span>Price : $20</span></span>
-                        <button className='card-add-to-cart-button'>
-                            <span className="card-text5">Add To Cart</span>
-                        </button>
-                    </div>
+                    
                 </div>
     </div>
   )
