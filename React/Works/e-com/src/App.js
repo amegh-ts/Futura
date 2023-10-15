4fad99693f94b592da936ded277e02eaf4550c8b
AMZ <80452444+amegh-amz@users.noreply.github.com>
2023-10-16 00:01:00 +0530

    16-10-2023

---
+++ React/Works/e-com/src/App.js
@@ -0,0 +1,51 @@
+import './App.css';
+import Main from './Components/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+
+
+
+
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
+        data-tag="font"
+      />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
+        data-tag="font"
+      />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap"
+        data-tag="font"
+      />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Pacifico:wght@400&amp;display=swap"
+        data-tag="font"
+      />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Pacifico:wght@400&amp;display=swap"
+        data-tag="font"
+      />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Pacifico:wght@400&amp;display=swap"
+        data-tag="font"
+      />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Pacifico:wght@400&amp;display=swap"
+        data-tag="font"
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ React/Works/e-com/src/Components/Main.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import './E-com.css'
+
+const Main = () => {
+    return (
+        <div>
+            
+            <div class="navbar-container">
+                <div class="navbar-navbar">
+                    <img
+                        src="public/external/rectangle113-jkb7-200h.png"
+                        alt="Rectangle113"
+                        class="navbar-rectangle1"
+                    />
+                    <div class="navbar-navseller">
+                        <img
+                            src="public/external/rectangle4119-9zf5-200h.png"
+                            alt="Rectangle4119"
+                            class="navbar-rectangle4"
+                        />
+                        <img
+                            src="public/external/store9eeae21112-85p7.svg"
+                            alt="Store9eeae21112"
+                            class="navbar-store9eeae21"
+                        />
+                        <span class="navbar-text"><span>Become a Seller</span></span>
+                    </div>
+                    <div class="navbar-nav-serach">
+                        <img
+                            src="public/external/rectangle319-83ls-200h.png"
+                            alt="Rectangle319"
+                            class="navbar-rectangle3"
+                        />
+                        <span class="navbar-text2"><span>Search</span></span>
+                        <img
+                            src="public/external/ellipse1122-84wso-200w.png"
+                            alt="Ellipse1122"
+                            class="navbar-ellipse1"
+                        />
+                    </div>
+                    <div class="navbar-nav-logo"></div>
+                    <div class="navbar-navcart">
+                        <img
+                            src="public/external/headercarteed1501126-oqok.svg"
+                            alt="headercarteed1501126"
+                            class="navbar-headercarteed1501"
+                        />
+                        <span class="navbar-text4"><span>Cart</span></span>
+                    </div>
+                    <div class="navbar-navprofile">
+                        <img
+                            src="public/external/ellipse2133-wu1m-200h.png"
+                            alt="Ellipse2133"
+                            class="navbar-ellipse2"
+                        />
+                        <span class="navbar-text6"><span>Kratos</span></span>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+            
+        </div>
+    )
+}
+
+export default Main
