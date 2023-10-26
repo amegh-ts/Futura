398faebbae87544ad83381339e589a18a1244048
AMZ <80452444+amegh-amz@users.noreply.github.com>
2023-10-26 13:12:54 +0530

    26-10-2023

--- React/Works/e-com/src/Components/Buy.jsx
+++ React/Works/e-com/src/Components/Buy.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Buy = () => {
+  const navigate=useNavigate()
+
+  useEffect(()=>{
+    const redirectToHome=()=>{
+      setTimeout(()=>{
+        navigate('/home')
+      },2000)
+    }
+    redirectToHome();
+  },[navigate]);
+  
   return (
     <div className='buy-main'>
       <div className="message-container">
-        <p className="message">Item purchased successfully</p>
+        <p className="message">Item Purchased <br/>Successfully</p>
       </div>
     </div>
   )
