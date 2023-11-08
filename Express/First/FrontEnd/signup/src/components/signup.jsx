507f0538a0b1b37dabf212b14f1b455660e8826a
AMZ <80452444+amegh-amz@users.noreply.github.com>
2023-11-09 00:58:54 +0530

    09-11-2023

--- Express/First/FrontEnd/signup/src/components/signup.jsx
+++ Express/First/FrontEnd/signup/src/components/signup.jsx
@@ -14,17 +14,22 @@ const Signup = () => {
     return (
         <div>
             <div>
-                <h2>SignUp</h2>
-                <div>
-                    <input type="text" placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} />
-                    <input type="text" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
-                    <input type="text" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
-                </div>
-                <div>
-                    <button onClick={display}>SignUp</button>
+                <div className='signup-container'>
+                    <div className='signup-form-container'>
+                        <h2>SignUp</h2>
+                        <div>
+                            <input type="text" placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} />
+                            <input type="text" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
+                            <input type="text" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                        </div>
+                        <div>
+                            <button onClick={display}>SignUp</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+
     )
 }
 
