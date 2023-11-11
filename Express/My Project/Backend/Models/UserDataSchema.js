57ed0b9369c54065cec7abec38ca857aa6fa350c
AMZ <80452444+amegh-amz@users.noreply.github.com>
2023-11-11 17:08:34 +0530

    11-11-2023

---
+++ Express/My Project/Backend/Models/UserDataSchema.js
@@ -0,0 +1,11 @@
+const mongoose=require('mongoose')
+
+const UserDataSchema=new mongoose.Schema({
+    firstname:{type:String,required:true},
+    lastname:{type:String,required:true},
+    dob:{type:String,required:true},
+    email:{type:String,required:true,unique:true},
+    phone:{type:String,required:true},
+    password:{type:String,required:true},
+
+},{timestamps})
