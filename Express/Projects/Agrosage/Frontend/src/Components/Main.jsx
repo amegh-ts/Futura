de27d1b1c1e59de06e5eeb583bef7891296ffba5
AMZ <80452444+amegh-amz@users.noreply.github.com>
2023-11-21 15:20:56 +0530

    21-11-2023

--- Express/Projects/Agrosage/Frontend/src/Components/Main.jsx
+++ Express/Projects/Agrosage/Frontend/src/Components/Main.jsx
@@ -5,6 +5,16 @@ import Login from './Login/Login';
 const Main = () => {
   return (
     <div className='body-main'> 
+    <div>L</div>
+    <div>jhe</div>
+    <div>efhb</div>
+    <div>waf</div>
+    <div>waf</div>
+    <div>wafaw</div>
+    <div>waf</div>
+    <div>awf</div>
+    <div>wfa</div>
+    <div>waf</div>
         <Login/>
     </div>
   )
