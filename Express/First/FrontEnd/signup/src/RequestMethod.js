20dd9f36cc7421a7e027c947c3bad12aa189d78e
AMZ <80452444+amegh-amz@users.noreply.github.com>
2023-11-25 14:24:20 +0530

    Update RequestMethod.js

--- Express/First/FrontEnd/signup/src/RequestMethod.js
+++ Express/First/FrontEnd/signup/src/RequestMethod.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/";
+// const BASE_URL = "http://localhost:5000/";
+const BASE_URL = "https://bot-1-x84j.onrender.com/";
 
 export const publicRequest=axios.create({
     baseURL:BASE_URL,
