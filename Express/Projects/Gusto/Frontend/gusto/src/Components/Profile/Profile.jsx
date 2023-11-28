6d60fa0026cda62e3347ee9198600233815e815b
AMZ <80452444+amegh-amz@users.noreply.github.com>
2023-11-28 22:26:22 +0530

    28-11-2023

--- Express/Projects/Gusto/Frontend/gusto/src/Components/Profile/Profile.jsx
+++ Express/Projects/Gusto/Frontend/gusto/src/Components/Profile/Profile.jsx
@@ -6,7 +6,13 @@ const Profile = () => {
     <section>
         <div className="profile-main">
             <div className='profile-container'>
-                <div className='profile-left'></div>
+                <div className='profile-left'>
+                  <div className='profile-left-img'>
+                    <img src="img/p1.png" alt="" />
+                    {/* <img src="img/r1.jpg" alt="" /> */}
+                  </div>
+                  <div className='profile-left-content'></div>
+                </div>
                 <div className='profile-right'></div>
             </div>
         </div>
