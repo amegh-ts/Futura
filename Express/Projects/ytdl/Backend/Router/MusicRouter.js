9890cf80427e20e9d4c1553dc66f37412df02514
AMZ <80452444+amegh-amz@users.noreply.github.com>
2023-12-04 12:04:44 +0530

    Update MusicRouter.js

--- Express/Projects/ytdl/Backend/Router/MusicRouter.js
+++ Express/Projects/ytdl/Backend/Router/MusicRouter.js
@@ -5,43 +5,42 @@ const ytdl = require('ytdl-core');
 router.use(express.json());
 
 router.post('/api/download', async (req, res) => {
-    const videoUrl = req.body.videoUrl;
+  const videoUrl = req.body.videoUrl;
 
-    try {
-        // Get video info
-        const videoInfo = await ytdl.getInfo(videoUrl);
-        const videoTitle = videoInfo.videoDetails.title;
-        const videoFormat = 'mp3';
+  try {
+    // Get video info
+    const videoInfo = await ytdl.getInfo(videoUrl);
+    const videoTitle = videoInfo.videoDetails.title;
 
-        // Download options
-        const downloadOptions = {
-            format: 'audioonly',
-            quality: 'highestaudio',
-            filter: 'audioonly',
-            highWaterMark: 1 << 25, // Set a high watermark to prevent issues with large downloads
-            requestOptions: {},
-        };
+    // Download options
+    const downloadOptions = {
+      format: 'audioonly',
+      quality: 'highestaudio',
+      filter: 'audioonly',
+      highWaterMark: 1 << 25, // Set a high watermark to prevent issues with large downloads
+      requestOptions: {},
+    };
 
-        // Check if the cookie header exists in the client's request
-        if (req.headers.cookie) {
-            downloadOptions.requestOptions.headers = {
-                cookie: req.headers.cookie,
-            };
-        }
-
-        // Create a readable stream
-        const audioStream = ytdl(videoUrl, downloadOptions);
-
-        // Set response headers
-        res.header('Content-Disposition', `attachment; filename="${videoTitle}.${videoFormat}"`);
-        res.header('Content-Type', 'audio/mpeg');
-
-        // Pipe the stream to the response
-        audioStream.pipe(res);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error downloading video');
+    // Check if the cookie header exists in the client's request
+    if (req.headers.cookie) {
+      downloadOptions.requestOptions.headers = {
+        cookie: req.headers.cookie,
+      };
     }
+
+    // Create a readable stream
+    const audioStream = ytdl(videoUrl, downloadOptions);
+
+    // Set response headers
+    res.header('Content-Disposition', `attachment; filename="${videoTitle}.mp3"`);
+    res.header('Content-Type', 'audio/mpeg');
+
+    // Pipe the stream to the response
+    audioStream.pipe(res);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error downloading video');
+  }
 });
 
 module.exports = router;
