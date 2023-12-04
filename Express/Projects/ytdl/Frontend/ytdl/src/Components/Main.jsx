58c9a52a772cd0f7c56383d52e9da3796bc7c0d5
AMZ <80452444+amegh-amz@users.noreply.github.com>
2023-12-04 12:13:43 +0530

    Update Main.jsx

--- Express/Projects/ytdl/Frontend/ytdl/src/Components/Main.jsx
+++ Express/Projects/ytdl/Frontend/ytdl/src/Components/Main.jsx
@@ -14,31 +14,40 @@ const Main = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      console.log('Request Payload:', { videoUrl }); // Log the payload
+      console.log('Request Payload:', { videoUrl });
       const result = await musicDl({ videoUrl });
       console.log('Download successful:', result);
   
-      if (result.success) {
-        // Create a Blob from the response data
-        const blob = new Blob([result.data], { type: 'audio/mpeg' });
-  
-        // Create a download link
-        const downloadLink = document.createElement('a');
-        downloadLink.href = URL.createObjectURL(blob);
-        downloadLink.download = `${result.filename}.mp3`;
-  
-        // Trigger a click on the link to start the download
-        document.body.appendChild(downloadLink);
-        downloadLink.click();
-        document.body.removeChild(downloadLink);
-      } else {
+      if (!result) {
         setError('Download failed. Please try again.');
+        return;
       }
+  
+      // Ensure that the result is a Blob or ArrayBuffer
+      const blob = new Blob([result], { type: 'audio/mpeg' });
+  
+      // Extract videoTitle from the response headers
+      const contentDisposition = result.headers['content-disposition'];
+      const match = contentDisposition.match(/filename="(.+?)"/);
+      const videoTitle = match ? match[1] : 'download';
+  
+      // Create a download link
+      const downloadLink = document.createElement('a');
+      downloadLink.href = URL.createObjectURL(blob);
+      downloadLink.download = `${videoTitle}.mp3`;
+  
+      // Trigger a click on the link to start the download
+      document.body.appendChild(downloadLink);
+      downloadLink.click();
+      document.body.removeChild(downloadLink);
     } catch (error) {
-      console.log('Error:', error);
+      console.error('Error:', error);
       setError('An error occurred. Please try again.');
     }
   };
+  
+  
+  
   
 
   return (
