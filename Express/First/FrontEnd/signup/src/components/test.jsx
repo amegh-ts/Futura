f60bad0032ed68675a37a790b2251819525031f0
AMZ <80452444+amegh-amz@users.noreply.github.com>
2023-12-08 14:18:08 +0530

    08-12-2023

--- Express/First/FrontEnd/signup/src/components/test.jsx
+++ Express/First/FrontEnd/signup/src/components/test.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux' 
 import { verifyTest } from './apiCall';
 
 
