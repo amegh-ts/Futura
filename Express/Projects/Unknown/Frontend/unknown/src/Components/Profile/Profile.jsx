6c8bd65dfd3aab122b30d30e689e1b4cad254457
AMZ <80452444+amegh-amz@users.noreply.github.com>
2023-12-09 00:26:18 +0530

    09-12-2023

--- Express/Projects/Unknown/Frontend/unknown/src/Components/Profile/Profile.jsx
+++ Express/Projects/Unknown/Frontend/unknown/src/Components/Profile/Profile.jsx
@@ -5,7 +5,16 @@ const Profile = () => {
   return (
     <profile>
       <div className='profile-main'>
-        <div className='profile-left'>fff</div>
+        <div className='profile-left'>
+          <div className='profile-left-image'>
+            <img src="/Images/p1.png" alt="" />
+          </div>
+          <div className='profile-left-details'>
+            <h2>Name</h2>
+            <h4>email</h4>
+            <h6>dob</h6>
+          </div>
+        </div>
         <div className='profile-right'>ss</div>
       </div>
     </profile>
