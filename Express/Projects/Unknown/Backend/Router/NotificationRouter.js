67a8a146b698fbbb4e0926f03ba6cdcb601f6543
AMZ <80452444+amegh-amz@users.noreply.github.com>
2023-12-10 17:53:39 +0530

    Update NotificationRouter.js

--- Express/Projects/Unknown/Backend/Router/NotificationRouter.js
+++ Express/Projects/Unknown/Backend/Router/NotificationRouter.js
@@ -2,6 +2,8 @@ const router = require('express').Router()
 const notification = require('../Models/NotificationSchema')
 const { verifyToken } = require('../VerifyToken');
 
+
+// send notification
 router.post('/sendNotification',verifyToken, async (req, res) => {
     // console.log('Postman data ?', req.body);
     const newNotification = new notification(req.body)
@@ -13,6 +15,15 @@ router.post('/sendNotification',verifyToken, async (req, res) => {
     }
 })
 
-
+// get notification
+router.get('/getNotification', verifyToken, async (req, res) => {
+    try {
+        const datas = await users.find()
+        console.log(datas);
+        res.status(200).json(datas)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
 
 module.exports = router
