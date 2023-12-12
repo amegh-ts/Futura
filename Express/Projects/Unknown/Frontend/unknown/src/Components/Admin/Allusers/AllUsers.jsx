3675236e283658d8fff5fe7e0cd9ca2ac90d8c15
AMZ <80452444+amegh-amz@users.noreply.github.com>
2023-12-13 01:08:43 +0530

    Update AllUsers.jsx

--- Express/Projects/Unknown/Frontend/unknown/src/Components/Admin/Allusers/AllUsers.jsx
+++ Express/Projects/Unknown/Frontend/unknown/src/Components/Admin/Allusers/AllUsers.jsx
@@ -21,48 +21,52 @@ const AllUsers = () => {
 
     }, [getUsers])
 
-    return (
-        <div>
-            <div className="all-users-main">
-                <div className="al-users-header">
-                    <div className='all-users-title'>
-                        <GrGroup className='users-icon' />
-                        <h3>Users</h3>
-                    </div>
-                    <div className='all-users-input'>
-                        <input type="text" />
-                        <IoSearch />
-                    </div>
-                </div>
-                <div className="all-users-body">
-                    {state.map((user, index) => (
-                        <div className="user-cards" key={index}>
+   // Inside the AllUsers component's return statement
+return (
+    <div className="all-users-main">
+        <div className="al-users-header">
+            <div className='all-users-title'>
+                <GrGroup className='users-icon' />
+                <h3>Users</h3>
+            </div>
+            <div className='all-users-input'>
+                <input type="text" />
+                <IoSearch />
+            </div>
+        </div>
+        <table className="all-users-table">
+            <thead>
+                <tr>
+                    <th>Image</th>
+                    <th>Username</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Type</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                {state.map((user, index) => (
+                    <tr key={index}>
+                        <td>
                             <div className="user-cards-img">
                                 <img src='/Images/p1.png' alt="" />
                             </div>
-                            <div>
-                                <p>{user.uname}</p>
-                            </div>
-                            <div>
-                                <p>{user.email}</p>
-                            </div>
-                            <div>
-                                <p>{user.phone}</p>
-                            </div>
-                            <div>
-                                <p>{user.type}</p>
-                            </div>
-                            <div>
-                                <button>edit</button>
-                            </div>
-                        </div>
-                    ))}
+                        </td>
+                        <td>{user.uname}</td>
+                        <td>{user.email}</td>
+                        <td>{user.phone}</td>
+                        <td>{user.type}</td>
+                        <td>
+                            <button>edit</button>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    </div>
+);
 
-
-                </div>
-            </div>
-        </div>
-    )
 }
 
 export default AllUsers
