03ed403754681a667e63aac658c9b7ef0141eeb1
AMZ <80452444+amegh-amz@users.noreply.github.com>
2023-12-21 10:47:53 +0530

    21-12-2023

--- Express/Projects/Unknown/Backend/Controller/appController.js
+++ Express/Projects/Unknown/Backend/Controller/appController.js
@@ -2,11 +2,11 @@ const nodemailer = require('nodemailer');
 const mailer = require('../Models/RecoverySchema');
 const dotenv = require('dotenv');
 
-const transporter=nodemailer.createTransport({
-    service:"gmail",
-    auth:{
-        user:process.env.EMAIL,
-        pass:process.env.EMAIL_PASSWORD
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASSWORD
     }
 })
 
@@ -31,17 +31,28 @@ const accountRecovery = async (req, res) => {
         await verification.save();
 
         //send otp via mail
-        const mailOption={
-            from:process.env.EMAIL,
-            to:email,
-            subject:'Your OTP Code',
-            text:`Your OTP code is: ${otp}`
+        const mailOption = {
+            from: process.env.EMAIL,
+            to: email,
+            subject: 'Your OTP Code',
+            html: `<div>
+                    <div style="text-align: center; background-color: #f0f0f0; padding: 20px;">
+                        <h1 style="color: #333;">Account Recovery OTP</h1>
+                        <p>Hello ${email},</p>
+                        <p>Your OTP code is:</p>
+                        <h2 style="color: #007bff;">${otp}</h2>
+                        <p>This code will expire in 5 minutes.</p>
+                        <img src="https://example.com/your-logo.png" alt="Your Logo" style="max-width: 200px; margin-top: 20px;">
+                        <p style="margin-top: 20px;">Thank you for using our service!</p>
+                    </div>
+                </div>
+            `,
         };
 
-        console.log("mail option",mailOption);
-        const info=await transporter.sendMail(mailOption);
-        console.log('Email sent:',info);
-        return res.status(200).json({message:"OTP sent successfully",otp})
+        console.log("mail option", mailOption);
+        const info = await transporter.sendMail(mailOption);
+        console.log('Email sent:', info);
+        return res.status(200).json({ message: "OTP sent successfully", otp })
     } catch (error) {
         console.error('Error saving verification details:', error);
         return res.status(500).json({ error: 'Error sending OTP' });
