1d908732b0f1480b67dfb3a6565a887c72e87c76
AMZ <80452444+amegh-amz@users.noreply.github.com>
2023-12-21 10:25:00 +0530

    21-12-2023

--- Express/Projects/Unknown/Backend/Controller/appController.js
+++ Express/Projects/Unknown/Backend/Controller/appController.js
@@ -2,6 +2,13 @@ const nodemailer = require('nodemailer');
 const mailer = require('../Models/RecoverySchema');
 const dotenv = require('dotenv');
 
+const transporter=nodemailer.createTransport({
+    service:"gmail",
+    auth:{
+        user:process.env.EMAIL,
+        pass:process.env.EMAIL_PASSWORD
+    }
+})
 
 // Account recovery
 console.log('check math.random ====', Math.random());
@@ -22,9 +29,22 @@ const accountRecovery = async (req, res) => {
     });
     try {
         await verification.save();
+
+        //send otp via mail
+        const mailOption={
+            from:process.env.EMAIL,
+            to:email,
+            subject:'Your OTP Code',
+            text:`Your OTP code is: ${otp}`
+        };
+
+        console.log("mail option",mailOption);
+        const info=await transporter.sendMail(mailOption);
+        console.log('Email sent:',info);
+        return res.status(200).json({message:"OTP sent successfully",otp})
     } catch (error) {
         console.error('Error saving verification details:', error);
-        return res.status(500).json({ error: 'Internal Server Error' });
+        return res.status(500).json({ error: 'Error sending OTP' });
     }
 };
 
