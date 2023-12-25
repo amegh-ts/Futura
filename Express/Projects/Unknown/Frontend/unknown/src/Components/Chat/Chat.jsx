c5c63c186123f28baf60a4d7edd48d8baa2f36fc
AMZ <80452444+amegh-amz@users.noreply.github.com>
2023-12-25 17:23:02 +0530

    Update Chat.jsx

--- Express/Projects/Unknown/Frontend/unknown/src/Components/Chat/Chat.jsx
+++ Express/Projects/Unknown/Frontend/unknown/src/Components/Chat/Chat.jsx
@@ -1,17 +1,43 @@
-import React from 'react'
-import './Chat.css'
+// Chat.js
+
+import React, { useState } from 'react';
+import './Chat.css';
 
 const Chat = () => {
-  return (
-    <div>
-        <div className="chat-main">
-            <div>
-                hfjdksl
-            </div>
-            <div>gfhjdksl</div>
-        </div>
-    </div>
-  )
-}
+  const [users, setUsers] = useState([
+    { id: 1, name: 'User 1' },
+    { id: 2, name: 'User 2' },
+    { id: 3, name: 'User 3' },
+    // Add more users as needed
+  ]);
 
-export default Chat
+  const [selectedUser, setSelectedUser] = useState(null);
+
+  const handleUserClick = (user) => {
+    setSelectedUser(user);
+    // You may want to load chat history or perform other actions here
+  };
+
+  return (
+    <div className="chat-container">
+      <div className="users-list">
+        <h2>Users</h2>
+        {users.map((user) => (
+          <div
+            key={user.id}
+            className={`user-item ${selectedUser && selectedUser.id === user.id ? 'active-user' : ''}`}
+            onClick={() => handleUserClick(user)}
+          >
+            {user.name}
+          </div>
+        ))}
+      </div>
+      <div className="chat-main">
+        <h2>{selectedUser ? `Chatting with ${selectedUser.name}` : 'Select a user to start chatting'}</h2>
+        {/* Add your chat components here */}
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
