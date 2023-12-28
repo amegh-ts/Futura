5c9e9623635eebaa71627c65641f1d463d8532f2
AMZ <80452444+amegh-amz@users.noreply.github.com>
2023-12-28 22:04:45 +0530

    Update MessageRouter.js

--- Express/Projects/Unknown/Backend/Router/MessageRouter.js
+++ Express/Projects/Unknown/Backend/Router/MessageRouter.js
@@ -3,8 +3,8 @@ const { createMessage, getMessages } = require('../Controller/MessageController'
 const router = require('express').Router()
 
 
-router.post('/',createMessage)
-router.get('/:chatId',getMessages)
+router.post('/', createMessage)
+router.get('/:chatId', getMessages)
 
 
-module.exports=router
+module.exports = router
