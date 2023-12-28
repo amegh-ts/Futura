0128990115fccad46c6a844a30789bf9cd0bf62d
AMZ <80452444+amegh-amz@users.noreply.github.com>
2023-12-28 22:01:52 +0530

    Update MessageController.js

--- Express/Projects/Unknown/Backend/Controller/MessageController.js
+++ Express/Projects/Unknown/Backend/Controller/MessageController.js
@@ -6,6 +6,27 @@ const createMessage= async(req,res)=>{
     const message=new messageSchema({
         chatId,senderId,text
     })
+
+    try {
+        const response=await message.save()
+        res.status(200).json(response)
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error)
+    }
 }
 
 // get message
+const getMessages=async(req,res)=>{
+    const {chatId}=req.params;
+    try {
+        const messages=await messageSchema.find({chatId})
+        res.status(200).json(messages)
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error)
+    }
+}
+
+
+module.exports = { createMessage,getMessages};
