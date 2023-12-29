473ef6783723ecd7993c3841788e0c832e5d0e27
AMZ <80452444+amegh-amz@users.noreply.github.com>
2023-12-29 11:03:18 +0530

    Update ChatBody.jsx

--- Express/Projects/Unknown/Frontend/unknown/src/Components/Chat/ChatBody.jsx
+++ Express/Projects/Unknown/Frontend/unknown/src/Components/Chat/ChatBody.jsx
@@ -1,28 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const ChatBody = ({ selectedChatId }) => {
-  console.log(selectedChatId);
+  const [message, setMessage] = useState('');
+
+  const handleMessageChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const handleSendMessage = () => {
+    // Handle sending the message (you can implement this logic)
+    console.log(`Sending message: ${message}`);
+    // Reset the input field after sending the message
+    setMessage('');
+  };
+
   return (
     <div className='chat-body-main'>
-      <p>chat with {selectedChatId}</p>
+      <p>Chat with {selectedChatId}</p>
       <div className="chat-body-container">
-        <ul class="message-list">
-          <li class="message sender-message">
-            <div class="message-bubble">Hello! This is a sender message.</div>
+        <ul className="message-list">
+          <li className="message sender-message">
+            <div className="message-bubble">Hello! This is a sender message.</div>
           </li>
-          <li class="message receiver-message">
-            <div class="message-bubble">Hi! This is a received message.</div>
+          <li className="message receiver-message">
+            <div className="message-bubble">Hi! This is a received message.</div>
           </li>
         </ul>
-
-
       </div>
-      <div class="input-container">
-        <input type="text" placeholder="Type your message..." />
-        <button>Send</button>
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="Type your message..."
+          value={message}
+          onChange={handleMessageChange}
+        />
+        <button onClick={handleSendMessage}>Send</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ChatBody
+export default ChatBody;
