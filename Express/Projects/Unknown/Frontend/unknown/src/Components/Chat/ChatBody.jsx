ede51c3241c6bd7c3205e21a19ed9295a93dde27
AMZ <80452444+amegh-amz@users.noreply.github.com>
2023-12-29 10:54:09 +0530

    29-12-2023

--- Express/Projects/Unknown/Frontend/unknown/src/Components/Chat/ChatBody.jsx
+++ Express/Projects/Unknown/Frontend/unknown/src/Components/Chat/ChatBody.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 
-const ChatBody = ({selectedChatId}) => {
+const ChatBody = ({ selectedChatId }) => {
   console.log(selectedChatId);
   return (
-    <div>ChatBody
-      <h2>{selectedChatId}</h2>
+    <div className='chat-body-main'>ChatBody
+      <div className="chat-body-container">
+        <ul class="message-list">
+          <li class="message sender-message">
+            <div class="message-bubble">Hello! This is a sender message.</div>
+          </li>
+          <li class="message receiver-message">
+            <div class="message-bubble">Hi! This is a received message.</div>
+          </li>
+        </ul>
+
+        <div class="input-container">
+          <input type="text" placeholder="Type your message..." />
+          <button>Send</button>
+        </div>
+      </div>
     </div>
   )
 }
