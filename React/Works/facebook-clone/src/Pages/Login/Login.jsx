66b63b194380be5e95925b8ee9f7a5499ec7798c
AMZ <80452444+amegh-amz@users.noreply.github.com>
2023-12-30 23:26:16 +0530

    30-12-2023

--- React/Works/facebook-clone/src/Pages/Login/Login.jsx
+++ React/Works/facebook-clone/src/Pages/Login/Login.jsx
@@ -7,39 +7,43 @@ const Login = () => {
             <div className="login-top">
                 <div className="login-top-left">
                     <span className="fb-logo">facebook</span>
-                    <span className="fb-p">Facebook helps you connect and share with the people in your life.</span>
+                    <span className="fb-p">Facebook helps you connect and share <br /> with the people in your life.</span>
                 </div>
                 <div className="login-top-right">
                     <div className="login-container">
                         <div className="login-container-top">
-                                <input type="text" placeholder="Email address or phone number"/>
-                                <input type="password" placeholder="Password"/>
+                            <input type="text" placeholder="Email address or phone number" />
+                            <input type="password" placeholder="Password" />
                             <div>
                                 <button>Log in</button>
                             </div>
-                        <span className="forgotten-pass">Forgotten password?</span>
+                            <span className="forgotten-pass">Forgotten password?</span>
 
                         </div>
                         <div className="login-container-bottom">
                             <button>Create new account</button>
                         </div>
                     </div>
+                    <span>Create a Page for a celebrity, brand or business.</span>
                 </div>
             </div>
             <div className="login-bottom">
-                <ul>
-                    <li>English (UK)</li>
-                    <li>മലയാളം</li>
-                    <li>தமிழ்</li>
-                    <li>ಕನ್ನಡ</li>
-                    <li>हिन्दी</li>
-                    <li>اردو</li>
-                    <li>বাংলা</li>
-                    <li>తెలుగు</li>
-                    <li>Español</li>
-                    <li>Português (Brasil)</li>
-                    <li>Français (France)</li>
-                </ul>
+                <div>
+                    <ul>
+                        <li>English (UK)</li>
+                        <li>മലയാളം</li>
+                        <li>தமிழ்</li>
+                        <li>ಕನ್ನಡ</li>
+                        <li>हिन्दी</li>
+                        <li>اردو</li>
+                        <li>বাংলা</li>
+                        <li>తెలుగు</li>
+                        <li>Español</li>
+                        <li>Português (Brasil)</li>
+                        <li>Français (France)</li>
+                    </ul>
+                </div>
+
             </div>
         </div>
     );
