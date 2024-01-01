e580a478093c56f324c262e01b637d2181433951
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-01 17:24:33 +0530

    01-01-2024

--- Express/Projects/TRVL/Frontend/trvl/src/Components/Users/Navbar/Navbar.jsx
+++ Express/Projects/TRVL/Frontend/trvl/src/Components/Users/Navbar/Navbar.jsx
@@ -3,8 +3,18 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='navbar-main'>
-        
+        <div className="nav-logo">
+        npm install react-icons --save
 
+        </div>
+        <div className="nav-menu">
+            <ul>
+                <li className="nav-list"></li>
+                <li className="nav-list"></li>
+                <li className="nav-list"></li>
+                <li className="nav-list"></li>
+            </ul>
+        </div>
     </div>
   )
 }
