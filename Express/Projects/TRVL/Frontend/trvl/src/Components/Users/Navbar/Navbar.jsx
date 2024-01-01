7ded8352d3513e8050c83f7a54f14798e6a6df34
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-01 19:04:04 +0530

    01-01-2023

--- Express/Projects/TRVL/Frontend/trvl/src/Components/Users/Navbar/Navbar.jsx
+++ Express/Projects/TRVL/Frontend/trvl/src/Components/Users/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import { GiFlamer } from "react-icons/gi";
 import { IoCloseCircleOutline } from "react-icons/io5";
@@ -6,23 +6,35 @@ import { CgMenuGridO } from "react-icons/cg";
 
 
 const Navbar = () => {
+    //  state to track navbar
+    const [navbar, setNavbar] = useState("nav-menu");
+
+    const showNavbar = () => {
+        setNavbar("nav-menu show-navbar")
+    }
+
+    const closeNavbar = () => {
+        setNavbar("nav-menu")
+    }
     return (
-        <div className='navbar-main'>
-            <div className="nav-logo">
-                <GiFlamer />
-                <h2>TRVL</h2>
+        <div className='navbar'>
+            <div className='navbar-main'>
+                <div className="nav-logo">
+                    <GiFlamer className='icon' />
+                    <h1>TRVL</h1>
+                </div>
+                <div className={navbar}>
+                    <ul>
+                        <li className="nav-list">Home</li>
+                        <li className="nav-list">Gallery</li>
+                        <li className="nav-list">Contact</li>
+                        <li className="nav-list">About Us</li>
+                    </ul>
+                    <IoCloseCircleOutline className='icon nav-close-icon' onClick={closeNavbar}/>
+                </div>
+                <button className='nav-menu-login btn'>Login</button>
+                <CgMenuGridO className='icon nav-menu-icon' onClick={showNavbar}/>
             </div>
-            <div className="nav-menu">
-                <ul>
-                    <li className="nav-list">Home</li>
-                    <li className="nav-list">Gallery</li>
-                    <li className="nav-list">Contact</li>
-                    <li className="nav-list">About Us</li>
-                </ul>
-                <IoCloseCircleOutline className='icon nav-menu-close' />
-            </div>
-            <button className='nav-menu-login btn'>Login</button>
-            <CgMenuGridO className='icon ' />
         </div>
     )
 }
