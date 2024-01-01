257974ef7d206ae035ac50ccb46991b93cf2a935
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-01 17:43:20 +0530

    01-01-2024

--- Express/Projects/TRVL/Frontend/trvl/src/Components/Users/Navbar/Navbar.jsx
+++ Express/Projects/TRVL/Frontend/trvl/src/Components/Users/Navbar/Navbar.jsx
@@ -1,22 +1,30 @@
 import React from 'react'
+import './Navbar.css'
+import { GiFlamer } from "react-icons/gi";
+import { IoCloseCircleOutline } from "react-icons/io5";
+import { CgMenuGridO } from "react-icons/cg";
+
 
 const Navbar = () => {
-  return (
-    <div className='navbar-main'>
-        <div className="nav-logo">
-        npm install react-icons --save
-
+    return (
+        <div className='navbar-main'>
+            <div className="nav-logo">
+                <GiFlamer />
+                <h2>TRVL</h2>
+            </div>
+            <div className="nav-menu">
+                <ul>
+                    <li className="nav-list">Home</li>
+                    <li className="nav-list">Gallery</li>
+                    <li className="nav-list">Contact</li>
+                    <li className="nav-list">About Us</li>
+                </ul>
+                <IoCloseCircleOutline className='icon nav-menu-close' />
+            </div>
+            <button className='nav-menu-login btn'>Login</button>
+            <CgMenuGridO className='icon ' />
         </div>
-        <div className="nav-menu">
-            <ul>
-                <li className="nav-list"></li>
-                <li className="nav-list"></li>
-                <li className="nav-list"></li>
-                <li className="nav-list"></li>
-            </ul>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
