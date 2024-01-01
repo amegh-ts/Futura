a21ecbb3a546ec4c9ee1cbe56dc8d3c4b34c54bb
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-01 22:41:49 +0530

    01-01-2024

--- Express/Projects/TRVL/Frontend/trvl/src/Components/Users/Content/Content.jsx
+++ Express/Projects/TRVL/Frontend/trvl/src/Components/Users/Content/Content.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import './Content.scss'
 
 const Content = () => {
-  return (
-    <div>Content</div>
-  )
+    return (
+        <div className='content'>
+            <div className='content-main'>
+                <div className="content-container">
+                    <div className="grid">
+                        <span className="flex">
+                            <h1>10</h1>
+                            <p>world of experience</p>
+                        </span>
+                        <span className="flex">
+                            <h1>10</h1>
+                            <p>world of experience</p>
+                        </span>
+                        <span className="flex">
+                            <h1>10</h1>
+                            <p>world of experience</p>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Content
