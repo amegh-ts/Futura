58509d49de964eed605eaae1cf164d1785a7b8e1
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-01 21:15:26 +0530

    01-01-2024

--- Express/Projects/TRVL/Frontend/trvl/src/Components/Users/Home/Home.jsx
+++ Express/Projects/TRVL/Frontend/trvl/src/Components/Users/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Home.scss'
 import bg1 from '../Assets/Videos/grass.mp4';
 import bg2 from '../Assets/Videos/cereal.mp4';
 import bg3 from '../Assets/Videos/rain.mp4';
@@ -7,8 +8,8 @@ import { AiOutlineSwapRight } from "react-icons/ai";
 const videoSources = [bg1, bg2, bg3];
 
 const getRandomVideoSource = () => {
-  const randomIndex = Math.floor(Math.random() * videoSources.length);
-  return videoSources[randomIndex];
+    const randomIndex = Math.floor(Math.random() * videoSources.length);
+    return videoSources[randomIndex];
 };
 
 
@@ -16,30 +17,48 @@ const getRandomVideoSource = () => {
 
 
 const Home = () => {
-  const randomVideoSource = getRandomVideoSource();
+    const randomVideoSource = getRandomVideoSource();
 
-  return (
-    <div className='home-main'>
-      <div className="home-bg">
-        <video src={randomVideoSource} autoPlay loop muted></video>
-      </div>
-      <div className="home-content">
-        <h1>Unloack your Travel dreams with us</h1>
-        <p>Discover the world's most adventurous nature. Life is so short for a trip.</p>
-        <button >Get started <AiOutlineSwapRight className='icon'/></button>
-      </div>
+    return (
+        <div className='home-main'>
+            <div className="home-bg">
+                <video src={randomVideoSource} autoPlay loop muted></video>
+            </div>
+            <div className="home-content">
+                <h1>Unlock your Travel dreams with us</h1>
+                <p>Discover the world's most adventurous nature. Life is so short for a trip.</p>
+                <button className='btn'>GET STARTED <AiOutlineSwapRight className='icon' /></button>
+            </div>
 
-      <div className="popular-places">
-        <div className="contents">
-            <h3>Popular places</h3>
-            <img src="" alt="" />
-            <img src="" alt="" />
-            <img src="" alt="" />
-            <img src="" alt="" />
+            <div className='home-card-main'>
+                <div className="home-card">
+                    <div className="card1">
+                        <label htmlFor="location">ABC</label>
+                        <input type="text" placeholder='dream location' />
+                    </div>
+                    <div className="card2">
+                        <label htmlFor="distance">ABC</label>
+                        <input type="text" placeholder='dream location' />
+                    </div>
+                    <div className="card3">
+                        <label htmlFor="price">ABC</label>
+                        <input type="text" placeholder='dream location' />
+                    </div>
+                    <button>Search</button>
+                </div>
+            </div>
+
+            <div className="popular-places">
+                <div className="contents">
+                    <h3>Popular places</h3>
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Home;
