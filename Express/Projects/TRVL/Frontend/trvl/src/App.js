bb7f414b71985a5ad11ac7e946cfae627071695e
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-01 17:18:11 +0530

    01-01-2024

--- Express/Projects/TRVL/Frontend/trvl/src/App.js
+++ Express/Projects/TRVL/Frontend/trvl/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Navbar from './Components/Users/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <div className="App-header">
-        
+        <Navbar/>
       </div>
     </div>
   );
---
+++ Express/Projects/TRVL/Frontend/trvl/src/Components/Users/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar-main'>
+        
+
+    </div>
+  )
+}
+
+export default Navbar
