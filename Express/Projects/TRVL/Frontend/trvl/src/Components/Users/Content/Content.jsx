e240b89fba83e278d155707fbdcc038d88a9a540
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-04 00:35:00 +0530

    Update Content.jsx

--- Express/Projects/TRVL/Frontend/trvl/src/Components/Users/Content/Content.jsx
+++ Express/Projects/TRVL/Frontend/trvl/src/Components/Users/Content/Content.jsx
@@ -3,7 +3,8 @@ import './Content.scss'
 
 const Content = () => {
     return (
-        <div className='content'>
+        <div>
+            <div className='content'>
             <div className='content-main'>
                 <div className="content-container">
                     <div className="grid">
@@ -12,21 +13,22 @@ const Content = () => {
                             <p>world of experience</p>
                         </span>
                         <span className="flex">
-                            <h1>10</h1>
-                            <p>world of experience</p>
+                            <h1>2K+</h1>
+                            <p>Fine Destination</p>
                         </span>
                         <span className="flex">
-                            <h1>10</h1>
-                            <p>world of experience</p>
+                            <h1>10+</h1>
+                            <p>Customer Reviews</p>
                         </span>
                         <span className="flex">
-                            <h1>10</h1>
-                            <p>world of experience</p>
+                            <h1>4.8</h1>
+                            <p>Overall Rating</p>
                         </span>
                     </div>
                 </div>
             </div>
             
+        </div>
         </div>
     )
 }
