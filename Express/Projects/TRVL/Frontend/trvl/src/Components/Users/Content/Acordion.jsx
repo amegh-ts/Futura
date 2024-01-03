21e8fb8a6cef3cdeedec636cb841d33ae18c8041
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-04 01:49:14 +0530

    Update Acordion.jsx

--- Express/Projects/TRVL/Frontend/trvl/src/Components/Users/Content/Acordion.jsx
+++ Express/Projects/TRVL/Frontend/trvl/src/Components/Users/Content/Acordion.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import { IoArrowDownCircleOutline } from "react-icons/io5";
 
 const Acordion = () => {
-  return (
-    <div className='acordion-container'>
-        <span className='title flex'>
-    
-        </span>
-        
-    </div>
-  )
+    return (
+        <div className='acordion-container'>
+            <span className='title flex'>
+                How do I chose the right place for travel?
+                <span>
+                    <IoArrowDownCircleOutline />
+                </span>
+            </span>
+
+        </div>
+    )
 }
 
 export default Acordion
