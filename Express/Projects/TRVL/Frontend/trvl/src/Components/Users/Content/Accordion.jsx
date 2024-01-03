3c0d63b3a4e0e170a5622732b0885a0a13566a9d
AMZ <80452444+amegh-ts@users.noreply.github.com>
2024-01-04 02:26:40 +0530

    04-01-2024

--- Express/Projects/TRVL/Frontend/trvl/src/Components/Users/Content/Accordion.jsx
+++ Express/Projects/TRVL/Frontend/trvl/src/Components/Users/Content/Accordion.jsx
@@ -11,7 +11,7 @@ const Accordion = () => {
 
   return (
     <div className={`accordion-container ${isExpanded ? 'activeTitle' : ''}`}>
-      <div className='title flex' onClick={toggleAccordion}>
+      {/* <div className='title flex' onClick={toggleAccordion}>
         How do I choose the right place for travel?
         <span>
           <IoArrowDownCircleOutline className={`icon ${isExpanded ? 'rotate' : ''}`} />
@@ -19,7 +19,7 @@ const Accordion = () => {
       </div>
       <p className={`description ${isExpanded ? 'show' : ''}`}>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit eius autem minima voluptas itaque iure, culpa quia odio veritatis? Repudiandae et neque dolor sapiente reiciendis accusantium minima asperiores obcaecati dolorem!
-      </p>
+      </p> */}
     </div>
   );
 };
